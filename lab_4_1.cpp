b5045dedffd698605e7209d92da0a0e427d5bc9c
bwaah <90904700+bwaah1@users.noreply.github.com>
2021-10-10 00:05:30 +0300

    Update lab_4_1.cpp

--- lab_4_1.cpp
+++ lab_4_1.cpp
@@ -14,7 +14,7 @@ int main()
 	i = 1;
 	while (i <= 15)
 	{
-		S += (sin(10 * i) + cos(10 / i)) / sqrt(i);
+		S += (sin(10 * i) + cos(10. / i)) / sqrt(i);
 		i++;
 	}
 	cout << S << endl;
@@ -23,7 +23,7 @@ int main()
 	i = 1;
 	do 
 	{
-		S += (sin(10 * i) + cos(10 / i)) / sqrt(i);
+		S += (sin(10 * i) + cos(10. / i)) / sqrt(i);
 		i++;
 	} while (i <= 15);
 	cout << S << endl;
@@ -31,14 +31,14 @@ int main()
 	S = 0;
 	for (i = 1; i <= 15; i++)
 	{
-		S += (sin(10 * i) + cos(10 / i)) / sqrt(i);
+		S += (sin(10 * i) + cos(10. / i)) / sqrt(i);
 	}
 	cout << S << endl;
 
 	S = 0;
 	for (i = 15; i >= 1; i--)
 	{
-		S += (sin(10 * i) + cos(10 / i)) / sqrt(i);
+		S += (sin(10 * i) + cos(10. / i)) / sqrt(i);
 	}
 	cout << S << endl;
 	return 0;
